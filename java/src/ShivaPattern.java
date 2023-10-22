1df1ac68494dd03dcd5393b61a6d3ed8e5acde3b
K.ShivaNagendra <shivaaman751@gmail.com>
2023-10-22 13:57:39 -0400

    Name pattern SHI

--- java/src/ShivaPattern.java
+++ java/src/ShivaPattern.java
@@ -1,5 +1,17 @@
 public class ShivaPattern {
     public static void main(String[] args) {
+        /*
+
+
+ * * * * *
+ * 
+ * * * * *
+         *
+ * * * * * 
+
+
+
+ */
         
             for (int i = 0 ; i < 3 ; i++){
                 for( int j = 0; j < 4 ; j++)
@@ -66,7 +78,47 @@ public class ShivaPattern {
             }
             System.out.println("");
         }
+System.out.println("");
 
+        /*
+
+
+* * * * * 
+    *
+    *
+    *
+* * * * *
+
+ */
+   for (int i = 0 ; i <= 4 ; i++ ){
+    for(int j = 0 ; j <= 4 ; j++){
+        if (i == 1 && j == 0 || i == 1 && j == 1 || i == 1 && j == 3 || i == 1 && j == 4  ){
+            System.out.print("  ");
+        }
+        else if (i == 2 && j == 0 || i == 2 && j == 1 || i == 2 && j == 3 || i == 2 && j == 4 )
+        {
+            System.out.print("  ");
+        }
+        else if (i == 3 && j == 0 || i == 3 && j == 1 || i == 3 && j == 3 || i == 3 && j == 4 )
+        {
+            System.out.print("  ");
+        }
+        else{
+            System.out.print("* ");
+        }
+        
+    }
+    System.out.println("");
+   }
+
+   System.out.println("");
+
+   /*
+   *            *
+     *        *
+       *    *
+          *
+    */
     }
 
     
