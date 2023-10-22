3e5541d26788c107a9e4e15618caf4c0ce7a5c86
K.ShivaNagendra <shivaaman751@gmail.com>
2023-10-22 01:31:00 -0400

    Name Pattern

---
+++ java/src/ShivaPattern.java
@@ -0,0 +1,73 @@
+public class ShivaPattern {
+    public static void main(String[] args) {
+        
+            for (int i = 0 ; i < 3 ; i++){
+                for( int j = 0; j < 4 ; j++)
+                {
+                    if (i == 0 || i == 2 || j == 0)
+                  System.out.print("* ");  
+                }
+                System.out.println("");
+            }
+            for (int k = 0 ; k < 2 ; k++ ){
+                for (int m = 0 ; m < 4; m++){
+                    if ( k == 1 || m == 3 ){
+                        System.out.print("* " );
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println("");
+            }
+            System.out.println();
+                /*     
+
+ *     *
+ *     *
+ * * * *
+ *     *
+ *     *
+ 
+ */
+
+         for(int i = 0 ; i <= 4 ; i ++ )
+        {
+            for(int j = 0 ; j <=3; j++)
+            {
+                if (i == 2){
+                    System.out.print("* ");
+                }
+                else if (i == 0 && j == 3 ){
+                    System.out.print("* ");
+                }
+                else if (i == 1 && j == 3 ){
+                    System.out.print("* ");
+                }
+                else if (i == 3 && j == 3 ){
+                    System.out.print("* ");
+                }
+                else if (i == 4 && j == 3 ){
+                    System.out.print("* ");
+                }
+                else if (i == 0 && j == 0 ){
+                    System.out.print("* ");
+                }
+                else if (i == 1 && j == 0 ){
+                    System.out.print("* ");
+                }
+                else if (i == 3 && j == 0 || i == 4 && j == 0 ){
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
+    
+}
