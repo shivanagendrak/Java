4b86d1e1f19c847473de1adcc7c47ba814594403
K.ShivaNagendra <shivaaman751@gmail.com>
2023-10-26 18:59:01 -0400

    prime

---
+++ java/src/prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        
+        int flag= 0;
+        for(int i = 2; i<number;i++){
+            if(number%i==0){
+            flag = 1;
+            }
+            
+            
+            
+        }
+        if (flag == 0){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not");
+        }
+    }
+    
+}
